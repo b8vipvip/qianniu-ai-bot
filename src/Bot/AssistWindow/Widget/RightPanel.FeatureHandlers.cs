551053703979ce7ad812d3d427bf8d5afd2fb4ef
b8vipvip <917931227@qq.com>
2026-07-08 11:29:26 +0800

    Add feature settings menu handler

---
+++ src/Bot/AssistWindow/Widget/RightPanel.FeatureHandlers.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Bot.Options;
+
+namespace Bot.AssistWindow.Widget
+{
+    public partial class RightPanel
+    {
+        private void menuFeatureSettings_Click(object sender, RoutedEventArgs e)
+        {
+            var item = sender as MenuItem;
+            var page = "知识库";
+            if (item != null)
+            {
+                page = (item.Tag ?? item.Header ?? page).ToString();
+            }
+            FeatureSettingsWindow.MyShow(Wnd, page);
+        }
+    }
+}
