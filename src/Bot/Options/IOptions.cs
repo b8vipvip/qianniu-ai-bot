fe078e58891333b357c182cb0194812f05d715c1
b8vipvip <917931227@qq.com>
2026-07-21 12:21:53 +0800

    refactor: keep option enum diff focused

--- src/Bot/Options/IOptions.cs
+++ src/Bot/Options/IOptions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,4 +39,5 @@ namespace Bot.Options
         InviteOrder,
         DataManagement
     }
+
 }
