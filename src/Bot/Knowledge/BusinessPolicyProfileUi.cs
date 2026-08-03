e06de14b6d994d12a7877b5af4c9730be8e69ac0
b8vipvip <917931227@qq.com>
2026-08-03 11:23:29 +0800

    fix: import text encoding namespace in policy editor

--- src/Bot/Knowledge/BusinessPolicyProfileUi.cs
+++ src/Bot/Knowledge/BusinessPolicyProfileUi.cs
@@ -6,6 +6,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
