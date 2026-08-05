91a219653085f0a269920c84f62712838b2afdfb
b8vipvip <917931227@qq.com>
2026-08-05 20:23:20 +0800

    feat: mirror scoped logs into shop log streams

--- src/BotLib/Log.cs
+++ src/BotLib/Log.cs
@@ -1,11 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BotLib
 {
@@ -28,7 +24,8 @@ namespace BotLib
 
         public static void WriteEnvironmentString(string tip)
         {
-            _writer.WriteEnvironmentString(tip);
+            Writer.WriteEnvironmentString(tip);
+            ScopedLogRouter.TryWrite("Environment", tip);
         }
 
         public static void Initiate(string FileName = "", bool saveLogByDay = false, int maxByte = 0, bool limitSameStringWriteCount = true)
@@ -37,18 +34,10 @@ namespace BotLib
             {
                 _writer.Close("启动程序");
             }
-            if (string.IsNullOrEmpty(FileName))
-            {
-                FileName = "txt";
-            }
-            if (!FileName.ToLower().EndsWith(".txt"))
-            {
-                FileName += ".txt";
-            }
+            if (string.IsNullOrEmpty(FileName)) FileName = "txt";
+            if (!FileName.ToLower().EndsWith(".txt")) FileName += ".txt";
             if (!FileName.Contains("\\"))
-            {
-                FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,FileName);
-            }
+                FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, FileName);
             _writer = new LogWriter(FileName, saveLogByDay, maxByte);
             _writer.LimitSameStringWriteCount = limitSameStringWriteCount;
         }
@@ -56,6 +45,7 @@ namespace BotLib
         public static void Assert(string msg)
         {
             Writer.Assert(msg);
+            ScopedLogRouter.TryWrite("Assert", msg);
         }
 
         public static string CurrentFileName
@@ -76,12 +66,16 @@ namespace BotLib
         public static void Error(string msg, object o, [System.Runtime.CompilerServices.CallerMemberName] string caller = "", [System.Runtime.CompilerServices.CallerFilePath] string path = "", [System.Runtime.CompilerServices.CallerLineNumber] int line = 0)
         {
             msg = msg + Environment.NewLine + "data=" + JsonConvert.SerializeObject(o);
-            Writer.Error(GetDesc(msg, caller, path, line));
+            var text = GetDesc(msg, caller, path, line);
+            Writer.Error(text);
+            ScopedLogRouter.TryWrite("ERROR", text);
         }
 
         public static void Error(string msg, [System.Runtime.CompilerServices.CallerMemberName] string caller = "", [System.Runtime.CompilerServices.CallerFilePath] string path = "", [System.Runtime.CompilerServices.CallerLineNumber] int line = 0)
         {
-            Writer.Error(GetDesc(msg, caller, path, line));
+            var text = GetDesc(msg, caller, path, line);
+            Writer.Error(text);
+            ScopedLogRouter.TryWrite("ERROR", text);
         }
 
         public static void ErrorWithMaxCount(string msg, int maxCount = 5, [System.Runtime.CompilerServices.CallerMemberName] string caller = "", [System.Runtime.CompilerServices.CallerFilePath] string path = "", [System.Runtime.CompilerServices.CallerLineNumber] int line = 0)
@@ -89,7 +83,9 @@ namespace BotLib
             string key = caller + line + path;
             if (IsLogCountLessThanMaxCount(key, maxCount + 1))
             {
-                Writer.Error(GetDesc(msg, caller, path, line));
+                var text = GetDesc(msg, caller, path, line);
+                Writer.Error(text);
+                ScopedLogRouter.TryWrite("ERROR", text);
             }
         }
 
@@ -97,55 +93,49 @@ namespace BotLib
         {
             int cnt = _errorWithMaxCountDict.GetOrAdd(key, 0);
             cnt++;
-            if (cnt < maxCount)
-            {
-                _errorWithMaxCountDict[key] = cnt;
-            }
+            if (cnt < maxCount) _errorWithMaxCountDict[key] = cnt;
             return cnt < maxCount;
         }
 
         private static string GetDesc(string msg, string caller, string path, int line)
         {
-            var idx = path.LastIndexOf('\\');
-            path = path.Substring(idx);
-            if (!msg.Contains(caller) && msg.Contains(path))
-            {
+            var idx = (path ?? string.Empty).LastIndexOf('\\');
+            if (idx >= 0) path = path.Substring(idx);
+            if (!msg.Contains(caller) && msg.Contains(path ?? string.Empty))
                 msg = string.Format("{0}\r\ncaller={1}, file={2},line={3}", msg.Trim(), caller, path, line);
-            }
             return msg;
         }
 
         private static string GetDesc(Exception e, string caller, string path, int line)
         {
-            var idx = path.LastIndexOf('\\');
-            path = path.Substring(idx);
+            var idx = (path ?? string.Empty).LastIndexOf('\\');
+            if (idx >= 0) path = path.Substring(idx);
             string text = e.ToString();
-            if (!text.Contains(caller) && text.Contains(path))
+            if (!text.Contains(caller) && text.Contains(path ?? string.Empty))
             {
                 text = string.Format("Message={0}\r\nBreakPoint={3}\r\ncaller={1}, file={2},line={3}\r\nStackTrace={4}",
-					text.Trim(),
-					caller,
-					path,
-					line,
-					e.StackTrace
-				);
+                    text.Trim(), caller, path, line, e.StackTrace);
             }
             return text;
         }
 
         public static void Exception(Exception e, [System.Runtime.CompilerServices.CallerMemberName] string caller = "", [System.Runtime.CompilerServices.CallerFilePath] string path = "", [System.Runtime.CompilerServices.CallerLineNumber] int line = 0)
         {
-            Writer.Exception(GetDesc(e, caller, path, line));
+            var text = GetDesc(e, caller, path, line);
+            Writer.Exception(text);
+            ScopedLogRouter.TryWrite("Exception", text);
         }
 
         public static void Info(string text)
         {
             Writer.Info(text);
+            ScopedLogRouter.TryWrite("Info", text);
         }
 
         public static void Debug(string text)
         {
             Writer.Debug(text);
+            ScopedLogRouter.TryWrite("Debug", text);
         }
 
         public static void Show()
@@ -155,7 +145,9 @@ namespace BotLib
 
         public static void TimeElapse(string title, DateTime t0)
         {
-            Writer.TimeElapse(title, t0);
+            var text = title + ",ms=" + (DateTime.Now - t0).TotalMilliseconds;
+            Writer.Info(text);
+            ScopedLogRouter.TryWrite("Info", text);
         }
 
         public static void WriteLine(string format, params object[] args)
@@ -164,6 +156,7 @@ namespace BotLib
             {
                 string msg = string.Format(format, args);
                 Writer.WriteLine(msg);
+                ScopedLogRouter.TryWrite("Line", msg);
             }
             catch (Exception e)
             {
@@ -187,10 +180,7 @@ namespace BotLib
             try
             {
                 Writer.CopyTo(fn);
-                if (!File.Exists(fn))
-                {
-                    Error("无法复制文件到:" + fn);
-                }
+                if (!File.Exists(fn)) Error("无法复制文件到:" + fn);
             }
             catch (Exception ex)
             {
@@ -200,6 +190,5 @@ namespace BotLib
             }
             return rt;
         }
-
     }
 }
